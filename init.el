53f3101c48615a40bac2de3041196ddf0a37b910
Graham Dobbins <gambyte@users.noreply.github.com>
2016-03-10 16:13:07 -0500

    Make C-a smarter
    Now it goes to indentation first and then alternates with beginning of line

--- init.el
+++ init.el
@@ -765,6 +765,26 @@ Don't mess with special buffers."
      (call-interactively #'isearch-backward)
      (recursive-edit))))
 
+(defun smarter-move-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
+
+Move point to the first non-whitespace character on this line.
+If point is already there, move to the beginning of the line.
+Effectively toggle between the first non-whitespace character and
+the beginning of the line.
+
+If ARG is not nil or 1, move forward ARG - 1 lines first.  If
+point reaches the beginning or end of the buffer, stop there."
+  (interactive "^p")
+  (setq arg (or arg 1))
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+
 (setq aw-keys (list ?h ?t ?n ?s ?a ?o ?e ?u))
 (setq avy-keys aw-keys)
 
@@ -859,6 +879,7 @@ Don't mess with special buffers."
 (global-set-key (kbd "C-z") #'repeat)
 (global-set-key (kbd "<f5>") #'egg-timer)
 (global-set-key (kbd "C-x c") #'server-edit)
+(global-set-key (kbd "C-a") #'smarter-move-beginning-of-line)
 
 (autoload 'mpc-resume "mpc")
 (autoload 'mpc-pause "mpc")
