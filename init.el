edee44d661d15e3029ff4229866d455ef8f9177c
Graham Dobbins <gambyte@users.noreply.github.com>
2016-05-07 17:41:27 -0400

    Remove superfluous autoload

--- init.el
+++ init.el
@@ -169,12 +169,6 @@ multiple functions can call each other in repetition."
 
 (global-set-key (kbd "M-#") #'wiktionary-word)
 
-(autoload 'dired-jump "dired-x"
-  "Jump to Dired buffer corresponding to current buffer." t)
-
-(autoload 'dired-jump-other-window "dired-x"
-  "Like \\[dired-jump] (dired-jump) but in other window." t)
-
 (global-set-key (kbd "C-x C-d") #'dired-jump)
 (global-set-key (kbd "C-x 4 C-d") #'dired-jump-other-window)
 
