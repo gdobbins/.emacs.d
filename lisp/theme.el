b155a09f36a5c559f4e35dba8ec3e5544dfbf243
Graham Dobbins <gambyte@users.noreply.github.com>
2016-05-10 07:03:32 -0400

    Fix solarized theme size changes

--- lisp/theme.el
+++ lisp/theme.el
@@ -7,11 +7,11 @@
 
 (setq solarized-use-variable-pitch nil)
 
-(setq solarized-height-minus-1 1)
-(setq solarized-height-plus-1 1)
-(setq solarized-height-plus-2 1)
-(setq solarized-height-plus-3 1)
-(setq solarized-height-plus-4 1)
+(setq solarized-height-minus-1 1.0)
+(setq solarized-height-plus-1 1.0)
+(setq solarized-height-plus-2 1.0)
+(setq solarized-height-plus-3 1.0)
+(setq solarized-height-plus-4 1.0)
 
 (setq solarized-scale-org-headlines nil)
 
