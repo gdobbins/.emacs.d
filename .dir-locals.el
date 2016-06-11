023a9c833a98f60a70fe11b2cd410e0fd2b2a701
Graham Dobbins <gambyte@users.noreply.github.com>
2016-06-11 06:36:32 -0400

    Add indent-tabs-mode and tab-width to dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,4 @@
-((nil . ((magit-commit-arguments . ("--gpg-sign=F1F2C078BAEE096B!")))))
+((nil . ((magit-commit-arguments . ("--gpg-sign=F1F2C078BAEE096B!"))
+	 (indent-tabs-mode . t)
+	 (tab-width . 8)
+	 (fill-column . 70))))
